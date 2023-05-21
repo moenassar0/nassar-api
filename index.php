06b443a131718ceb3782635bd86eb7fc77071545
Mohamad Nassar <mnassar57@gmail.com>
2023-05-21 23:37:56 +0300

    Initial routes mapper/handler

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 use App\Heart\Router;
+use App\Controllers\UserController;
 
 //Add routes
 $router = new Router();
 $router->get("/users", UserController::class, "getUsers");
-$router->post("/user", UserController::class, "addUser");
+
+//Find if route requested matches one of our routes
+$url = (parse_url($_SERVER['REQUEST_URI']));
+$url = $url['path'];
+$method = $_SERVER['REQUEST_METHOD'];
+for($x = 0; $x < count($router->routes); $x++){
+    if($router->routes[$x]['path'] === $url && $router->routes[$x]['method'] === $method){
+        $className = $router->routes[$x]['controller'];
+        $class = new $className();
+        $functionName = $router->routes[$x]['function'];
+        $result = $class->$functionName();
+        echo $result;
+    }
+}
