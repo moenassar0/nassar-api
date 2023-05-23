01a0aee3d3eeed907ee43f7e892592d70aac2804
Mohamad Nassar <mnassar57@gmail.com>
2023-05-23 18:04:17 +0300

    Refactor into general function

--- Heart/Router.php
+++ Heart/Router.php
@@ -5,15 +5,19 @@ class Router{
     public $routes = [];
 
     public function get(string $path, string $controller, string $functionName){
-        array_push($this->routes, array(
-            "path" => $path,
-            "method" => "GET",
-            "controller" => $controller,
-            "function" => $functionName
-        ));
+        $this->addRoute("GET", $path, $controller, $functionName);
     }
 
     public function post(){
         return 0;
+    }
+
+    public function addRoute(string $method, string $path, string $controller, string $functionName){
+        array_push($this->routes, array(
+            "path" => $path,
+            "method" => $method,
+            "controller" => $controller,
+            "function" => $functionName
+        ));
     }
 }
