be3b54cd6bc06218328d29409ff8e1c1b270ec08
Mohamad Nassar <mnassar57@gmail.com>
2023-05-24 23:48:30 +0300

    Subquery example

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -6,13 +6,15 @@ use App\Heart\CamelDatabase;
 class UserController{
     public function getUsers(){
         $db = new CamelDatabase();
-        print_r(
-            $db
-                ->select(array("id", "firstName"))
-                ->from("users")
-                ->where("id", "=", "0")
-                ->orderBy('id')
-                ->execute()
-        );
+        $whereINQuery = $db->select("user_id")->from("mock_data")->where("user_id", ">", "980")->andWhere("id", ">", "900")->andWhere("id", "<", "950");
+        
+        $db = new CamelDatabase();
+        $data = $db->select("*")->from("users")->whereIn("id", $whereINQuery)->execute();
+
+        echo json_encode($data);
+    }
+
+    public function postUser(){
+
     }
 }
