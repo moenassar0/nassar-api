0b5ac82f003bfc3d7ad9bd2bbffa88a305a95d19
Mohamad Nassar <mnassar57@gmail.com>
2023-05-26 02:55:40 +0300

    Example middleware grouping

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . './');
 $dotenv->load();
 
 $router = new Router();
-$router->get("/users", UserController::class, "getUsers");
-$router->post("/user", UserController::class, "postUser");
+
+$router->middleware('auth', function($router) {
+    $router->get('/users', UserController::class, 'getUsers');
+    $router->post("/user", UserController::class, "postUser");
+});
+
 $router->route();
