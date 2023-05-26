6b9b0e5fcd028b5b34b1a0199573fd3719e1222a
Mohamad Nassar <mnassar57@gmail.com>
2023-05-26 03:13:30 +0300

    Throw exception for missing middleware

--- Heart/Middleware.php
+++ Heart/Middleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Heart;
 use App\Middlewares\Auth;
+use Exception;
 
 class Middleware{
     const MAP = [
@@ -9,7 +10,10 @@ class Middleware{
     ];
 
     public static function resolve($key){
-        $middleware = static::MAP[$key];
-        (new $middleware)->handle();
+        if(isset(static::MAP[$key])){
+            $middleware = static::MAP[$key];
+            (new $middleware)->handle();
+        }
+        else throw new Exception("Middleware " . $key . " is failing! Not found class or middleware key.");
     }
 }
